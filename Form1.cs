eae638ec6441d720abc3eacd27eb9810876ff044
gabriel.giulianelli07@gmail.com <gabriel.giulianelli07@gmail.com>
2025-04-25 15:53:46 +0200

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2425.GabrielGiulianelli._4G.esMeteo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
